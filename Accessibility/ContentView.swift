941e294c69e532e5c7f14fc7b9dc47451d5da371
Brandon Knox <bknox.dev@gmail.com>
2021-05-26 11:45:58 -0700

    Initial Commit

---
+++ Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Brandon Knox on 5/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
