75880e1e8c416809fed400fc10f57b17970fe315
Brandon Knox <bknox.dev@gmail.com>
2021-05-26 12:29:22 -0700

    Customized text for voiceover to more explicitly narrate the number along with "out of 5"

--- Accessibility/ContentView.swift
+++ Accessibility/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+            .accessibility(value: Text("\(rating) out of 5"))
     }
 }
 
